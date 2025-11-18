c0eb30bb30f2b1663f56d30af6d8caad19e35c36
[dyad] <git@dyad.sh>
2025-11-18 11:53:36 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-c6c6fb7c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qqq13tsgdmo"
+    "revision": "0.8v0t1agts1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
