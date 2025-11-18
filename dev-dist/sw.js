c0abfa25cf0cce80cb1762c2a2ebdd0054f72a3d
[dyad] <git@dyad.sh>
2025-11-18 10:14:21 -0300

    [dyad] Tentativa final de correção da chave de API do Supabase - wrote 1 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-c6c6fb7c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m5663trk99"
+    "revision": "0.3ogrd44p28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
