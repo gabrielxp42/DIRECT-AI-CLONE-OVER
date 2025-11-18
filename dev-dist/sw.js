20d5b84c762897ce4650899da6a335e330caa872
[dyad] <git@dyad.sh>
2025-11-18 13:31:57 -0300

    [dyad] Garantindo que o cliente Supabase seja sempre um objeto exportado para evitar erros de 'undefined' na inicialização do módulo. - wrote 1 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-c6c6fb7c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ebtaa90orcg"
+    "revision": "0.n71qg8p7ucg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
